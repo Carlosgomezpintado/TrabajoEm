6b6eec6f45db7f78bb2062d53af5b7d878cdd8cf
adriglez <47420437+adriglez@users.noreply.github.com>
2019-04-03 16:13:54 +0200

    Delete script inserts.sql
    Borrado

--- script inserts.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `pfc`.`convocatoria` (`idCONVOCATORIA`, `actual`) VALUES ('ordinaria', b'1');
-INSERT INTO `pfc`.`curso` (`Cod_CURSO`, `actual`) VALUES ('2016', b'1');
-INSERT INTO `pfc`.`examen` (`Cod_Ex`) VALUES ('ordinaria2016');
-INSERT INTO `pfc`.`grupo_clase` (`Cod_GC`) VALUES ('1');
-INSERT INTO `pfc`.`grupo_clase` (`Cod_GC`) VALUES ('2');
-INSERT INTO `pfc`.`practica` (`Cod_Practica`) VALUES ('ordinaria2016');
-INSERT INTO `pfc`.`tipo_evaluacion` (`Ev_Continua`, `Peso_Ex`, `Peso_Pr`, `Peso_Ev_C`) VALUES ('1', '50', '25', '25');
